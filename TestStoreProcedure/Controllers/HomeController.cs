d6651e525d4bbef34bbce69d479277d07458cf1a
danny2904 <dannythanhfore@gmail.com>
2024-03-26 07:37:03 +0700

    Add project files.

---
+++ TestStoreProcedure/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using TestStoreProcedure.Models;
+
+public class HomeController : Controller
+{
+    private QLCCHCEntities db = new QLCCHCEntities(); // Tên của bạn ADO.NET Entity Data Model
+
+    public ActionResult Index()
+    {
+        List<TTHCModel> tthcs = db.sp_GetAllTTHC().Select(t => new TTHCModel
+        {
+            MaTTHC = t.IdTTHC,
+            TenTTHC = t.NameTTHC,
+            CQTThucHien = t.CQTThucHien,
+            VanBanQuyDinh = t.VanBanQuyDinh,
+            QuyetDinhCongBo = t.QuyetDinhCongBo,
+            MucDoDVCTT = t.MucDoDVCTT,
+            TichHopCongDVCQG = t.TichHopCongDVCQG
+        }).ToList();
+
+        return View(tthcs);
+    }
+}
---
+++ TestStoreProcedure/Models/TTHCModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestStoreProcedure.Models
+{
+    public class TTHCModel
+    {
+        public string MaTTHC { get; set; }
+        public string TenTTHC { get; set; }
+        public string CQTThucHien { get; set; }
+        public string VanBanQuyDinh { get; set; }
+        public string QuyetDinhCongBo { get; set; }
+        public string MucDoDVCTT { get; set; }
+        public string TichHopCongDVCQG { get; set; }
+    }
+}
